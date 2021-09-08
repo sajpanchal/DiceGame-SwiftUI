fe7f25ccd6fbd49c1c8a33ec9be4da9e09486eb8
Saj Panchal <sajpanchal@gmail.com>
2021-09-08 13:11:22 -0400

    added dice with values and button to roll it will animation.

--- DiceGame/ContentView.swift
+++ DiceGame/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var diceValues = [1,2,3,4,5,6]
+    @State var diceValue: Int = 0
+    @State var angle: Double = 0.0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(diceValue == 0 ? "Roll dice": "\(diceValue)")")
+                .frame(width: 100, height: 100, alignment: .center)
+                .cornerRadius(3.0)
+                .background(Color.pink)
+                .padding()
+                .font(.largeTitle)
+                .rotation3DEffect(
+                   
+                        .degrees(angle)
+                        
+                    ,
+                    axis: (x: 1.0, y: 1.0, z: 1.0)
+                )
+                
+            Button(action: {
+              
+                withAnimation {
+                    self.angle += 360.0
+                    diceValue = diceValues.shuffled()[0]
+                }
+            }, label: {
+                
+                Text("Roll")
+                
+            })
+        }
+        
+        
     }
 }
 
