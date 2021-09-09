2a9980cfd9b6c7a7f10483d809c6f5da740376d6
Saj Panchal <sajpanchal@gmail.com>
2021-09-09 14:24:33 -0400

    modified Dice. removed button and used onTapGesture with dice view.

--- DiceGame/ContentView.swift
+++ DiceGame/ContentView.swift
@@ -12,12 +12,13 @@ struct ContentView: View {
     @State var diceValue: Int = 0
     @State var angle: Double = 0.0
     @State var selectedTab = 0
+    @State var generator = UINotificationFeedbackGenerator()
     var results = Result()
     var body: some View {
         TabView {
             VStack {
-                Text("\(diceValue == 0 ? "Roll dice": "\(diceValue)")")
-                    .frame(width: 100, height: 100, alignment: .center)
+                Text("\(diceValue == 0 ? "Roll it": "\(diceValue)")")
+                    .frame(width: 150, height: 150, alignment: .center)
                     .cornerRadius(3.0)
                     .background(Color.pink)
                     .cornerRadius(16)
@@ -29,26 +30,7 @@ struct ContentView: View {
                         
                         ,
                         axis: (x: 1.0, y: 1.0, z: 1.0)
-                    )
-                
-                Button(action: {
-                    
-                    withAnimation {
-                        self.angle += 720.0
-                        diceValue = diceValues.shuffled()[0]
-                    }
-                    results.scores.append(diceValue)
-                    let formatter = DateFormatter()
-                    formatter.dateStyle = .short
-                    formatter.timeStyle = .long
-                    results.currentDate.append(formatter.string(from: Date()))
-                    
-                    
-                }, label: {
-                    
-                    Text("Roll")
-                    
-                })
+                    ).onTapGesture(perform:rollDice)
             }
             .tabItem {
                 Image(systemName:"play")
@@ -65,6 +47,22 @@ struct ContentView: View {
      
         
         
+    }
+    func rollDice() {
+        withAnimation {
+            self.angle += 720.0
+            diceValue = diceValues.shuffled()[0]
+            simpleSuccess()
+        }
+        results.scores.append(diceValue)
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .long
+        results.currentDate.append(formatter.string(from: Date()))
+    }
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
     }
 }
 
